5bd9de368ddc44857654d992a76a22c59fc8570e
Aliseth <charlotte.saidi@outlook.fr>
2023-12-18 14:24:38 +0100

    update build workflow & docker-compose

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Examen <code>Intégration-Déploiement continu</code>.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/charlottesaidi/exam-oc-web"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Lien vers le répo
         </a>
       </header>
     </div>
